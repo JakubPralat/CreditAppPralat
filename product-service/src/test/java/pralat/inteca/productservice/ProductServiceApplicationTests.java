7f2b8967ec6aca69f4467036bffe9cf30f1b348c
KubaPralat <jakub.pralat@student.put.poznan.pl>
2021-07-11 14:39:45 +0200

    Spring boots initialized

---
+++ product-service/src/test/java/pralat/inteca/productservice/ProductServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package pralat.inteca.productservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
